a046e333be371e1bc1886e94d01aee5a7443ae57
yograj <yogasandhu@gmail.com>
2021-07-21 18:41:02 -0400

    MVVM and data class commit

--- app/src/main/java/com/simrnpuri/newsapp/ui/MainActivity.kt
+++ app/src/main/java/com/simrnpuri/newsapp/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.simrnpuri.newsapp
+package com.simrnpuri.newsapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.simrnpuri.newsapp.R
 
 class MainActivity : AppCompatActivity() {
     public override fun onCreate(savedInstanceState: Bundle?) {
